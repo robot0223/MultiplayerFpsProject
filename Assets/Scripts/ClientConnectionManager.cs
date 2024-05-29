18849928ee0caf61801157d42a37c5dadcf95ad9
robot0223 <fydy0223@gmail.com>
2024-05-29 12:35:25 +0900

    Added ClientConnectionManager again

--- Assets/Scripts/ClientConnectionManager.cs
+++ Assets/Scripts/ClientConnectionManager.cs
@@ -1,4 +1,6 @@
-﻿using TMPro;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using Unity.Entities;
 using Unity.NetCode;
 using Unity.Networking.Transport;
@@ -6,116 +8,114 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-namespace TMG.NFE_Tutorial
+public class ClientConnectionManager : MonoBehaviour
 {
-    public class ClientConnectionManager : MonoBehaviour
+    // [SerializeField] private TMP_InputField _addressField;
+    // [SerializeField] private TMP_InputField _portField;
+    [SerializeField] private TMP_Dropdown _connectionModeDropdown;
+    [SerializeField] private TMP_Dropdown _teamDropdown;
+    [SerializeField] private Button _connectButton;
+
+    private ushort Port => ushort.Parse("7979");
+    private string Address => "127.0.0.1";
+
+    private void OnEnable()
     {
-       // [SerializeField] private TMP_InputField _addressField;
-       // [SerializeField] private TMP_InputField _portField;
-        [SerializeField] private TMP_Dropdown _connectionModeDropdown;
-        [SerializeField] private TMP_Dropdown _teamDropdown;
-        [SerializeField] private Button _connectButton;
-        
-        private ushort Port => ushort.Parse("7979");
-        private string Address => "127.0.0.1";
+        _connectionModeDropdown.onValueChanged.AddListener(OnConnectionModeChanged);
+        _connectButton.onClick.AddListener(OnButtonConnect);
+        OnConnectionModeChanged(_connectionModeDropdown.value);
+    }
 
-        private void OnEnable()
+    private void OnDisable()
+    {
+        _connectionModeDropdown.onValueChanged.RemoveAllListeners();
+        _connectButton.onClick.RemoveAllListeners();
+    }
+
+    private void OnConnectionModeChanged(int connectionMode)
+    {
+        string buttonLabel;
+        _connectButton.enabled = true;
+
+        switch (connectionMode)
         {
-            _connectionModeDropdown.onValueChanged.AddListener(OnConnectionModeChanged);
-            _connectButton.onClick.AddListener(OnButtonConnect);
-            OnConnectionModeChanged(_connectionModeDropdown.value);
+            case 0:
+                buttonLabel = "Start Host";
+                break;
+            case 1:
+                buttonLabel = "Start Server";
+                break;
+            case 2:
+                buttonLabel = "Start Client";
+                break;
+            default:
+                buttonLabel = "<ERROR>";
+                _connectButton.enabled = false;
+                break;
         }
 
-        private void OnDisable()
+        var buttonText = _connectButton.GetComponentInChildren<TextMeshProUGUI>();
+        buttonText.text = buttonLabel;
+    }
+
+    private void OnButtonConnect()
+    {
+        DestroyLocalSimulationWorld();
+        SceneManager.LoadScene("Level_00_Main");
+
+        switch (_connectionModeDropdown.value)
         {
-            _connectionModeDropdown.onValueChanged.RemoveAllListeners();
-            _connectButton.onClick.RemoveAllListeners();
-        }
-
-        private void OnConnectionModeChanged(int connectionMode)
-        {
-            string buttonLabel;
-            _connectButton.enabled = true;
-            
-            switch (connectionMode)
-            {
-                case 0:
-                    buttonLabel = "Start Host";
-                    break;
-                case 1 : 
-                    buttonLabel = "Start Server";
-                    break;
-                case 2:
-                    buttonLabel = "Start Client";
-                    break;
-                default:
-                    buttonLabel = "<ERROR>";
-                    _connectButton.enabled = false;
-                    break;
-            }
-
-            var buttonText = _connectButton.GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = buttonLabel;
-        }
-
-        private void OnButtonConnect()
-        {
-            DestroyLocalSimulationWorld();
-            SceneManager.LoadScene("Level_00_Main");
-            
-            switch (_connectionModeDropdown.value)
-            {
-                case 0:
-                    StartServer();
-                    StartClient();
-                    break;
-                case 1:
-                    StartServer();
-                    break;
-                case 2:
-                    StartClient();
-                    break;
-                default:
-                    Debug.LogError("Error: Unknown connection mode", gameObject);
-                    break;
-            }
-        }
-
-        private static void DestroyLocalSimulationWorld()
-        {
-            foreach (var world in World.All)
-            {
-                if (world.Flags == WorldFlags.Game)
-                {
-                    world.Dispose();
-                    break;
-                }
-            }
-        }
-
-        private void StartServer()
-        {
-            var serverWorld = ClientServerBootstrap.CreateServerWorld("Server World");
-
-            var serverEndpoint = NetworkEndpoint.AnyIpv4.WithPort(Port);
-            {
-                using var networkDriverQuery = serverWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
-                networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Listen(serverEndpoint);
-            }
-        }
-
-        private void StartClient()
-        {
-            var clientWorld = ClientServerBootstrap.CreateClientWorld("Client World");
-
-            var connectionEndpoint = NetworkEndpoint.Parse(Address, Port);
-            {
-                using var networkDriverQuery = clientWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
-                networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Connect(clientWorld.EntityManager, connectionEndpoint);
-            }
-            
-            World.DefaultGameObjectInjectionWorld = clientWorld;
-            
+            case 0:
+                StartServer();
+                StartClient();
+                break;
+            case 1:
+                StartServer();
+                break;
+            case 2:
+                StartClient();
+                break;
+            default:
+                Debug.LogError("Error: Unknown connection mode", gameObject);
+                break;
         }
     }
+
+    private static void DestroyLocalSimulationWorld()
+    {
+        foreach (var world in World.All)
+        {
+            if (world.Flags == WorldFlags.Game)
+            {
+                world.Dispose();
+                break;
+            }
+        }
+    }
+
+    private void StartServer()
+    {
+        var serverWorld = ClientServerBootstrap.CreateServerWorld("Server World");
+
+        var serverEndpoint = NetworkEndpoint.AnyIpv4.WithPort(Port);
+        {
+            using var networkDriverQuery = serverWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+            networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Listen(serverEndpoint);
+        }
+    }
+
+    private void StartClient()
+    {
+        var clientWorld = ClientServerBootstrap.CreateClientWorld("Client World");
+
+        var connectionEndpoint = NetworkEndpoint.Parse(Address, Port);
+        {
+            using var networkDriverQuery = clientWorld.EntityManager.CreateEntityQuery(ComponentType.ReadWrite<NetworkStreamDriver>());
+            networkDriverQuery.GetSingletonRW<NetworkStreamDriver>().ValueRW.Connect(clientWorld.EntityManager, connectionEndpoint);
+        }
+
+        World.DefaultGameObjectInjectionWorld = clientWorld;
+
+    }
 }
+
