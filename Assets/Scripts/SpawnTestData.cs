08a2f91081769bcb710b25f8d1cf00db3bae7bf4
robot0223 <fydy0223@gmail.com>
2024-05-31 12:54:42 +0900

    made spawner test data

---
+++ Assets/Scripts/SpawnTestData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class SpawnTestData : IComponentData
+{
+    GameObject Spawnprefab;
+}
